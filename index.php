c4e4c31128a0c51503da8f0ef89062d38ac80fea
sejrusak <sejrus01@seznam.cz>
2018-04-06 01:03:40 +0200

    Fin

--- index.php
+++ index.php
@@ -5,7 +5,9 @@ include 'header.php';
 <img id="index_image" src="grafika/marvel.jpg" alt="marvel_image"/>
 <br />
 <p>
-Doufáme, že si u nás vyberete film, který se vám bude líbit a budete se k nám vracet. Pokud si chcete zarezervovat film, tak se musíte zaregistrovat, ale prohlížet můžete i bez toho. Těšíme se na vyši další návštěvu a přejeme hezký den.
+  Marvel Studios je americké televizní a filmové studio sídlící v Beverly Hills v Kalifornii. Bylo založeno v 90. letech 20. století mateřskou společností Marvel Entertainment,
+  do které patří také vydavatelství komiksů Marvel Comics. Produkuje především filmová zpracování komiksů mateřské společnosti. Patří do divize The Walt Disney Studios společnosti The Walt Disney Company.</p>
+<p>Doufáme, že si u nás vyberete film, který se vám bude líbit a budete se k nám vracet. Pokud si chcete zarezervovat film, tak se musíte zaregistrovat, ale prohlížet můžete i bez toho. Těšíme se na vyši další návštěvu a přejeme hezký den.
 </p>
 
 
