78c61a3f8cb4ffbdcdbaf395b8446d68c48c8cd5
sejrusak <sejrus01@seznam.cz>
2018-04-06 01:02:26 +0200

    Finito

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 
 <?php
 include 'header.php';
+?>
+<img id="index_image" src="grafika/marvel.jpg" alt="marvel_image"/>
+<br />
+<p>
+Doufáme, že si u nás vyberete film, který se vám bude líbit a budete se k nám vracet. Pokud si chcete zarezervovat film, tak se musíte zaregistrovat, ale prohlížet můžete i bez toho. Těšíme se na vyši další návštěvu a přejeme hezký den.
+</p>
 
+
+
+<?php
 include 'footer.php';
 ?>
