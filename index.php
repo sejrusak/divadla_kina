ac8a53474d2278cbb6a4693fa19369d59b0ae20e
sejrusak <sejrus01@seznam.cz>
2018-04-06 09:16:24 +0200

    Fin

--- index.php
+++ index.php
@@ -2,14 +2,24 @@
 <?php
 include 'header.php';
 ?>
-<img id="index_image" src="grafika/marvel.jpg" alt="marvel_image"/>
-<br />
-<p>
-  Marvel Studios je americké televizní a filmové studio sídlící v Beverly Hills v Kalifornii. Bylo založeno v 90. letech 20. století mateřskou společností Marvel Entertainment,
-  do které patří také vydavatelství komiksů Marvel Comics. Produkuje především filmová zpracování komiksů mateřské společnosti. Patří do divize The Walt Disney Studios společnosti The Walt Disney Company.</p>
+<h1>Divadla/Kina</h1>
 <p>Doufáme, že si u nás vyberete film, který se vám bude líbit a budete se k nám vracet. Pokud si chcete zarezervovat film, tak se musíte zaregistrovat, ale prohlížet můžete i bez toho. Těšíme se na vyši další návštěvu a přejeme hezký den.
 </p>
 
+<h2>Představení</h2>
+<img id="index_image" src="grafika/disney_marvel.jpg" alt="marvel_image"/>
+
+
+
+<p>
+  Marvel Studios je americké televizní a filmové studio sídlící v Beverly Hills v Kalifornii. Bylo založeno v 90. letech 20. století mateřskou společností Marvel Entertainment,
+  do které patří také vydavatelství komiksů Marvel Comics. Produkuje především filmová zpracování komiksů mateřské společnosti. Patří do divize The Walt Disney Studios společnosti The Walt Disney Company.</p>
+
+<p>
+  Walter Elias „Walt“ Disney (5. prosince 1901, Hermosa, Chicago, Illinois, USA – 15. prosince 1966, Burbank, Kalifornie, USA) byl americký filmový producent, režisér, scenárista, dabér, animátor, podnikatel a filantrop. Ve své době silně ovlivnil zábavní průmysl. Jako zakladatel (společně se svým bratrem Royem O. Disneyem) firmy Walt Disney Productions se stal jedním z nejznámějších filmových producentů na světě. Společnost, kterou založil, se v současnosti jmenuje The Walt Disney Company a má roční obrat asi 35 miliard dolarů.
+
+  Znám je jako filmový producent a bavič, zároveň však také jako inovátor animovaného filmu a zábavních parků. Společně se zaměstnanci společnosti vytvořil řadu světově známých postaviček včetně Mickey Mouse, kterému Disney propůjčil i vlastní hlas. Vyhrál 26 Oskarů z 59 nominací, včetně rekordních čtyř Oskarů v jednom roce,[2] čímž v tomto oboru získal více cen a nominací než kdokoli jiný. Také obdržel sedm ocenění Emmy. Po něm pojmenované zábavní parky můžeme najít ve Spojených státech amerických (Disneyland, Walt Disney World Resort), v Japonsku (Tokyo Disney Resort), Francii (Disneyland Paris) a Číně (Hong Kong Disneyland). Zemřel na rakovinu plic v kalifornském Burbanku.
+</p>
 
 
 <?php
